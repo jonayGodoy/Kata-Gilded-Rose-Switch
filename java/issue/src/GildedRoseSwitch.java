dff1cee05406842748ea065fa3eafc69a3c13be9
jonayGodoy <jonaydam2@gmail.com>
2017-06-04 07:31:16 +0100

    aged brie add gilded rose switch

--- java/issue/src/GildedRoseSwitch.java
+++ java/issue/src/GildedRoseSwitch.java
@@ -9,22 +9,30 @@ public class GildedRoseSwitch {
     public void updateQuality() {
         for (Item item : items) {
          switch (item.getName()){
-
-
+             case "Aged Brie":
+                if(item.getSellIn() >= 0){
+                    if(item.getQuality() < 50) {item.setQuality(item.getQuality() + 1);}
+                }else{
+                    updateItemGenericQuality(item);
+                }
+                 break;
              default:
                  item.setSellIn(item.getSellIn()-1);
-                 if(item.getQuality() > 0){
-                     if(item.getSellIn() <= 0){
-                         item.setQuality(item.getQuality()-2);
-                     }else{
-                         item.setQuality(item.getQuality()-1);
-                     }
-                 }
+                 updateItemGenericQuality(item);
                  break;
          }
         }
     }
 
+    private void updateItemGenericQuality(Item item) {
+        if(item.getQuality() > 0){
+            if(item.getSellIn() <= 0){
+                item.setQuality(item.getQuality()-2);
+            }else{
+                item.setQuality(item.getQuality()-1);
+            }
+        }
+    }
 
 
 }
