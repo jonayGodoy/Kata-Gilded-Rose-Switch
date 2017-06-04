9efeaca535f04525ed2dd2c2e6b0276b192de2e6
jonayGodoy <jonaydam2@gmail.com>
2017-06-04 20:41:57 +0100

    add mocha test

---
+++ javascript/test/issue/GildedRose.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let should = require('chai').should();
+let expect = require('chai').expect;
+
+describe("Gilded Rose",function(){
+    it('spec name', function () {
+        true.should.equal(true);
+    });
+});
