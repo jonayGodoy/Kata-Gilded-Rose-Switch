e47df6a6397c51c56836f16a448fbfa304dfa100
jonayGodoy <jonaydam2@gmail.com>
2017-06-08 02:18:27 +0100

    extracting backstage from the super environment

--- javascript/src/solution/Backstage.js
+++ javascript/src/solution/Backstage.js
@@ -1,38 +1,38 @@
 const Item = require('./Item');
+const FactoryBackStage = (() => {
+    let sellInKey;
+    let qualityKey;
 
-let sellInKey;
-let qualityKey;
+    return class Backstage extends Item {
 
-class Backstage extends Item{
-
-    constructor(name, sellIn, quality){
-        super(name,sellIn, quality);
-        sellInKey = Object.getOwnPropertySymbols(this)[1];
-        qualityKey = Object.getOwnPropertySymbols(this)[2];
-    }
-
-    updateSellIn(){
-        super.updateSellIn();
-        if(this[sellInKey] <= 0){
-            this[qualityKey] = 0;
+        constructor(name, sellIn, quality) {
+            super(name, sellIn, quality);
+            sellInKey = Object.getOwnPropertySymbols(this)[1];
+            qualityKey = Object.getOwnPropertySymbols(this)[2];
         }
-    };
 
-    updateQuality(){
-        if(this[sellInKey] <= 0){
-            this[sellInKey] = 0;
-        }else {
-            if (this[sellInKey] <= 5) {
-                if (this[qualityKey] < 50) {
-                    this[qualityKey] = this[qualityKey] +3;
-                }
-            } else if (this[sellInKey] <= 10) {
-                if (this[qualityKey] < 50) {
-                    this[qualityKey] = this[qualityKey] +2;
+        updateSellIn() {
+            super.updateSellIn();
+            if (this[sellInKey] <= 0) {
+                this[qualityKey] = 0;
+            }
+        };
+
+        updateQuality() {
+            if (this[sellInKey] <= 0) {
+                this[sellInKey] = 0;
+            } else {
+                if (this[sellInKey] <= 5) {
+                    if (this[qualityKey] < 50) {
+                        this[qualityKey] = this[qualityKey] + 3;
+                    }
+                } else if (this[sellInKey] <= 10) {
+                    if (this[qualityKey] < 50) {
+                        this[qualityKey] = this[qualityKey] + 2;
+                    }
                 }
             }
-        }
-    };
-}
-
-module.exports = Backstage;
+        };
+    }
+});
+module.exports = FactoryBackStage();
