13b51da9b5ee7fbfbc6a835fa445c0a81b8c8ef2
jonayGodoy <jonaydam2@gmail.com>
2017-06-08 03:14:14 +0100

    conjured work with protected

--- javascript/src/solution/Conjured.js
+++ javascript/src/solution/Conjured.js
@@ -1,16 +1,16 @@
 const Item = require('./Item');
 
 class Conjured extends Item{
-
     constructor(name, sellIn, quality){
         super(name,sellIn, quality);
-        let qualityKey = Object.getOwnPropertySymbols(this)[2];
         let updateQualityKey = Object.getOwnPropertySymbols(this)[4];
 
-        this[updateQualityKey] = function updateQuality(){
-         //   Item[updateQualityKey]();
+        const updateQualityParent = this[updateQualityKey].bind(this);
 
-            this[qualityKey] = this[qualityKey] - 2;
+        this[updateQualityKey] = function updateQuality(){
+            /* example super() with method private */
+            updateQualityParent();
+            updateQualityParent();
         };
     }
 }
